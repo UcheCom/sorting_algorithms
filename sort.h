73745b8143a28a678b7c8fd26ce98cdff98a095d
gloriaadanna <gloriaagwam7@gmail.com>
2023-09-24 05:11:30 -0700

    corr

--- sort.h
+++ sort.h
@@ -4,6 +4,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - this is thge enumeration of boolean values
+ * @false: equals 0
+ * @true: equals 1
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+/**
+ * struct listint_s - this is a doubly linked list
+ * @n: integer
+ * @prev: previous pointer
+ * @next: next element pointer
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
