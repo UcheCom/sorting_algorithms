a9d522f7d86ec78936b71e16aa0be4e827878f96
gloriaadanna <gloriaagwam7@gmail.com>
2023-09-24 05:16:16 -0700

    corr

--- sort.h
+++ sort.h
@@ -31,10 +31,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* print help functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 #endif
