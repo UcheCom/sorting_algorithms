85449ddc8ee5ef13d33dd065e35d64e53ce89499
gloriaadanna <gloriaagwam7@gmail.com>
2023-09-24 05:23:22 -0700

    corr

--- sort.h
+++ sort.h
@@ -3,7 +3,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "sort.h"
 
+/* comparison direction */
 #define UP 0
 #define DOWN 1
 
@@ -40,5 +42,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
+
 
 #endif
