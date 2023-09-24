ffe7db4be7fb197f7955e9bd6e77b818c2270e07
gloriaadanna <gloriaagwam7@gmail.com>
2023-09-24 05:03:31 -0700

    corr

--- sort.h
+++ sort.h
@@ -1,9 +1,13 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 #endif
