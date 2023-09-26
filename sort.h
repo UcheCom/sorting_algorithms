9b2eff92493263c19121c5a169d9f56d6eee86a2
Uchenna Oko <uche4newton1981@gmail.com>
2023-09-26 23:50:18 +0100

    Header File Updated

--- sort.h
+++ sort.h
@@ -49,5 +49,7 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void swap_ints(int *a, int *b);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
